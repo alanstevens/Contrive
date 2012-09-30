b78976935acf1a9cfb2b8bab7bbd61708849e76b
Alan Stevens <alanstevens@gmail.com>
2012-09-30 09:59:06 -0400

    Completed the .As<T>() extension method.

--- src/Contrive.Common/Extensions/ObjectExtensions.cs
+++ src/Contrive.Common/Extensions/ObjectExtensions.cs
@@ -8,7 +8,7 @@ namespace Contrive.Common.Extensions
     [DebuggerStepThrough]
     public static bool IsTypeOf<T>(this object value)
     {
-      return value.GetType() == typeof(T);
+      return value.GetType() == typeof (T);
     }
 
     [DebuggerStepThrough]
@@ -40,28 +40,66 @@ namespace Contrive.Common.Extensions
     {
       var result = default(T);
 
-      // given an empty string, return the default value
       if (value is String && value.ToString().Blank()) return result;
 
       try
       {
+        if (result is String) return (T) (Convert.ToString(value) as object);
+
         if (result is ValueType)
         {
-          // TODO: HAS 07/10/2011 Complete tests for all default value types.
-          if (result is Int32) return (T) (Convert.ToInt32(value) as object);
-          if (result is Double) return (T) (Convert.ToDouble(value) as object);
-          if (result is Decimal) return (T) (Convert.ToDecimal(value) as object);
           if (result is Boolean) return (T) (Convert.ToBoolean(value) as object);
+          if (result is Byte) return (T) (Convert.ToByte(value) as object);
+          if (result is Char) return (T) (Convert.ToChar(value) as object);
+          if (result is Decimal) return (T) (Convert.ToDecimal(value) as object);
+          if (result is Double) return (T) (Convert.ToDouble(value) as object);
+          if (result is Int16) return (T) (Convert.ToInt16(value) as object);
+          if (result is Int32) return (T) (Convert.ToInt32(value) as object);
+          if (result is Int64) return (T) (Convert.ToInt64(value) as object);
+          if (result is SByte) return (T) (Convert.ToSByte(value) as object);
+          if (result is Single) return (T) (Convert.ToSingle(value) as object);
+          if (result is UInt16) return (T) (Convert.ToUInt16(value) as object);
+          if (result is UInt32) return (T) (Convert.ToUInt32(value) as object);
+          if (result is UInt64) return (T) (Convert.ToUInt64(value) as object);
           if (result is Guid) return (T) (new Guid(value.ToString()) as object);
-          if (typeof(T) == typeof(DateTime)) return (T) (DateTime.Parse(value.ToString()) as object);
+          if (result is DateTime)
+          {
+            try
+            {
+              return (T) (Convert.ToDateTime(value) as object);
+            }
+            catch (InvalidCastException)
+            {
+              return (T) (DateTime.Parse(value.ToString()) as object);
+            }
+          }
         }
         result = (T) value;
       }
-      catch (Exception ex)
+      catch (InvalidCastException ex)
       {
-        typeof(ObjectExtensions).LogException(ex);
+        LogException(ex);
       }
+      catch(FormatException ex)
+      {
+        LogException(ex);
+      }
+      catch(ArgumentNullException ex)
+      {
+        LogException(ex);
+      }
+      catch(OverflowException ex)
+      {
+        LogException(ex);
+      }
+      
       return result;
+    }
+
+    static void LogException(Exception ex)
+    {
+      // Log the exception and return the default value.
+      typeof (ObjectExtensions).LogException(ex);
     }
   }
 }
